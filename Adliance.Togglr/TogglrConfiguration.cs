611e50b4d5460cf27b3d3e08eb940db5247ddfc4
Christoph Kopf <christoph.kopf@adliance.net>
2020-09-25 20:47:24 +0200

    add parser configuration

---
+++ Adliance.Togglr/TogglrConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace Adliance.Togglr
+{    
+    [Verb("generate-report", true, HelpText = "Generate a report with specified configuration")]
+    public class TogglrConfiguration
+    {
+        [Option('c', "configuration", Required = false, Default = "configuration.json", HelpText = "Path to configuration file")] public string ConfigurationFilePath { get; set; } = "";
+    }
+}
