0881a916989b374705186e6784f4df9b700ef96f
kopfrechner <ckopf86@gmail.com>
2020-10-01 19:59:36 +0200

    Use recommended verb generate-configuration

--- Adliance.Togglr/TogglrConfigurationGeneratorParameters.cs
+++ Adliance.Togglr/TogglrConfigurationGeneratorParameters.cs
@@ -2,7 +2,7 @@
 
 namespace Adliance.Togglr
 {
-    [Verb("configuration-generator", HelpText = "Generate a template 'configuration.json' in the current folder")]
+    [Verb("generate-configuration", HelpText = "Generate a template 'configuration.json' in the current folder")]
     public class TogglrConfigurationGeneratorParameters 
     {
         [Option('t', "targetFileName", Required = false, Default = "configuration.json", HelpText = "Target filename of the configuration file")] public string ConfigurationFilePath { get; set; } = "";
